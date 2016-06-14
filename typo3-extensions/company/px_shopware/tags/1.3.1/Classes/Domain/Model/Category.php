7944693c18d9332391b254664f4baa13a157d298
a.wuttig <a.wuttig@3d3db68c-a071-48f4-bd9d-a78c148a4cb8>
2016-06-14 14:02:59 +0000

    px_shopware * creates tag 1.3.1 from trunk

---
+++ typo3-extensions/company/px_shopware/tags/1.3.1/Classes/Domain/Model/Category.php
@@ -0,0 +1,239 @@
+<?php
+namespace Portrino\PxShopware\Domain\Model;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2016 Andre Wuttig <wuttig@portrino.de>, portrino GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+use Portrino\PxShopware\Backend\Form\Wizard\SuggestEntryInterface;
+use Portrino\PxShopware\Backend\Hooks\ItemEntryInterface;
+use Portrino\PxShopware\Service\Shopware\AbstractShopwareApiClientInterface;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\ObjectStorage;
+
+/**
+ * Class Category
+ *
+ * @package Portrino\PxShopware\Domain\Model
+ */
+class Category extends AbstractShopwareModel implements SuggestEntryInterface, ItemEntryInterface {
+
+    /**
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * @var \TYPO3\CMS\Core\Http\Uri
+     */
+    protected $uri = '';
+
+    /**
+     * @var \Portrino\PxShopware\Service\Shopware\CategoryClientInterface
+     * @inject
+     */
+    protected $categoryClient;
+
+    /**
+     * @var \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\Portrino\PxShopware\Domain\Model\Category>
+     */
+    protected $path;
+
+    /**
+     * Category constructor.
+     *
+     * @param $raw
+     * @param $token
+     */
+    public function __construct($raw, $token) {
+        parent::__construct($raw, $token);
+
+        if (isset($this->raw->name)) {
+            $this->setName($this->raw->name);
+        }
+        if (isset($this->raw->pxShopwareUrl)) {
+            $this->setUri($this->raw->pxShopwareUrl);
+        }
+        $this->initStorageObjects();
+    }
+
+    /**
+     * Initializes all \TYPO3\CMS\Extbase\Persistence\ObjectStorage properties.
+     *
+     * @return void
+     */
+    protected function initStorageObjects() {
+        $this->path = new ObjectStorage();
+    }
+
+    /**
+     *
+     */
+    public function initializeObject() {
+
+    }
+
+    /**
+     * @return \TYPO3\CMS\Extbase\Persistence\ObjectStorage<\Portrino\PxShopware\Domain\Model\Category>
+     */
+    public function getSubCategories () {
+        return $this->categoryClient->findByParent($this->id);
+    }
+
+    /**
+     * @return string
+     */
+    public function getName() {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    /**
+     * @return \TYPO3\CMS\Core\Http\Uri
+     */
+    public function getUri() {
+        return $this->uri;
+    }
+
+    /**
+     * @param \TYPO3\CMS\Core\Http\Uri|string $uri
+     */
+    public function setUri($uri) {
+        if (is_string($uri)) {
+            $uri = new \TYPO3\CMS\Core\Http\Uri($uri);
+        }
+        $this->uri = $uri;
+    }
+
+    /**
+     * @return ObjectStorage
+     */
+    public function getPath() {
+        return $this->path;
+    }
+
+    /**
+     * @param ObjectStorage $path
+     */
+    public function setPath($path) {
+        $this->path = $path;
+    }
+
+    /**
+     * Adds a path element
+     *
+     * @param \Portrino\PxShopware\Domain\Model\Category $pathElement
+     *
+     * @return void
+     */
+    public function addPathElement(\Portrino\PxShopware\Domain\Model\Category $pathElement) {
+        $this->path->attach($pathElement);
+    }
+
+    /**
+     * Removes a path
+     *
+     * @param \Portrino\PxShopware\Domain\Model\Path $pathElementToRemove The path element to be removed
+     *
+     * @return void
+     */
+    public function removePathElement(\Portrino\PxShopware\Domain\Model\Category $pathElementToRemove) {
+        $this->path->detach($pathElementToRemove);
+    }
+
+    /**
+     * @param bool $includeSelf TRUE if this element should be included in bread crumb path, FALSE if not
+     *
+     * @return mixed
+     */
+    public function getBreadCrumbPath($includeSelf = TRUE) {
+
+        if (isset($this->getRaw()->path) && $this->getRaw()->path != '') {
+            $pathArray = array_reverse(GeneralUtility::trimExplode('|', $this->getRaw()->path, TRUE));
+            foreach ($pathArray as $pathItem) {
+                /** @var Category|NULL $pathElement */
+                $pathElement = $this->categoryClient->findById($pathItem);
+                if ($pathElement) {
+                    $this->addPathElement($pathElement);
+                }
+            }
+        }
+
+        /** @var array $path */
+        $path = array_map(function($item) {
+            return $item->getName();
+        }, $this->getPath()->toArray());
+        if ($includeSelf === TRUE) {
+            array_push($path, $this->getName());
+        }
+        return implode('/', $path);
+    }
+
+    /**
+     * @return int
+     */
+    public function getSelectItemId() {
+        return $this->getId();
+    }
+
+    /**
+     * @return string
+     */
+    public function getSelectItemLabel() {
+        return $this->getName() . ' [' . $this->getId() . ']';
+    }
+
+    /**
+     * @return int
+     */
+    public function getSuggestId() {
+        return $this->getId();
+    }
+
+    /**
+     * @return string
+     */
+    public function getSuggestLabel() {
+        return $this->getName() . ' [' . $this->getId() . ']';
+    }
+
+    /**
+     * @return string
+     */
+    public function getSuggestDescription() {
+        return $this->getBreadCrumbPath(FALSE);
+    }
+
+    /**
+     * @return string
+     */
+    public function getSuggestIconIdentifier() {
+        return 'px-shopware-category';
+    }
+
+}
---
+++ typo3-extensions/company/px_shopware/tags/1.3.1/Documentation/ForAdministrators/Index.rst
@@ -0,0 +1,69 @@
+﻿.. ==================================================
+.. FOR YOUR INFORMATION
+.. --------------------------------------------------
+.. -*- coding: utf-8 -*- with BOM.
+
+.. include:: ../Includes.txt
+
+
+.. _admin-manual:
+
+For Administrators
+==================
+
+
+Import
+------
+
+There are two ways of installing the extension. Import the extension to your server from the
+
+- TYPO3 Extension Repository (TER) or
+- via GIT
+
+From TER (Classic Mode)
+^^^^^^^^^^^^^^^^^^^^^^^
+
+**Download Zip-File**
+
+The extension could also be downloaded and installed from https://typo3.org/extensions/repository/view/px_shopware
+
+.. figure:: ../Images/ForAdministrators/px_shopware-install-zip-file_2.png
+    :width: 500px
+    :align: left
+
+    Please use the icon "*Upload Extension .t3x / .zip*" above the dropdown.
+
+.. figure:: ../Images/ForAdministrators/px_shopware-install-zip-file_4.png
+    :width: 500px
+    :align: left
+
+    After that a flash message that shopware extension was installed successfully should be visble.
+
+Via composer
+^^^^^^^^^^^^
+
+Since TYPO3 7.x you are able to get extension via composer. As described on https://wiki.typo3.org/Composer#Composer_Mode_ you just have to user TYPO3 in Composer Mode
+and add this line to your require section within the composer.json file and run composer install / composer update.
+
+.. code-block:: json
+
+    "typo3-ter/px-shopware": "dev-master",
+
+If you want a specific version than change "dev-master" to the version you need.
+
+Installation
+------------
+
+Wether you run your TYPO3 in Classic Mode or Composer Mode you should install the extension via ExtensionManager or via Composer. Click `here <https://wiki.typo3.org/Composer>`_ for more details
+
+After the installation is finished open the Extension Configuration by clicking on the "Configure" gear.
+
+.. toctree::
+   :maxdepth: 5
+   :titlesonly:
+   :glob:
+
+   ExtensionManager/Index
+   TypoScript/Index
+
+.. _composer: https://getcomposer.org/
