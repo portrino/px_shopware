45d27b66e0d08443650b63662730a5d442e66c57
a.wuttig <a.wuttig@3d3db68c-a071-48f4-bd9d-a78c148a4cb8>
2016-06-15 10:13:54 +0000

    px_shopware * enables solr indexing for shopware articles ** adds `IndexQueueCommandController` to populate Solr IndexQueue ** adds different `SolrIndexer` to index specific shopware resources

--- typo3-extensions/company/px_shopware/trunk/ext_emconf.php
+++ typo3-extensions/company/px_shopware/trunk/ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
     'uploadfolder' => false,
     'createDirs' => '',
     'clearCacheOnLoad' => 0,
-    'version' => '1.3.1',
+    'version' => '1.4.0',
     'constraints' =>
     array (
         'depends' =>
