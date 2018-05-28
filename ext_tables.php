45c150b2ff075f50e325e9eeb18ababb56c9c4c7
Axel Böswetter <boeswetter@portrino.de>
2018-05-28 15:36:12 +0200

    [CLEANUP] updates ext_localconf and ext_tables

--- ext_tables.php
+++ ext_tables.php
@@ -1,20 +1,23 @@
 <?php
 defined('TYPO3_MODE') || die();
 
-call_user_func(function ($_EXTKEY) {
+$boot = function ($_EXTKEY) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
         $_EXTKEY,
         'Configuration/TypoScript',
         'Shopware Integration'
     );
-}, 'px_shopware');
 
-if (TYPO3_MODE == "BE") {
-    $GLOBALS['TBE_STYLES']['skins'][$_EXTKEY] = [
-        'name' => $_EXTKEY,
-        'stylesheetDirectories' => [
-            'structure' => '', //removes structure stylesheet
-            'visual' => 'EXT:' . $_EXTKEY . '/Resources/Public/Css' // changes default directory
-        ]
-    ];
-}
+    if (TYPO3_MODE === 'BE') {
+        $GLOBALS['TBE_STYLES']['skins'][$_EXTKEY] = [
+            'name' => $_EXTKEY,
+            'stylesheetDirectories' => [
+                'structure' => '', //removes structure stylesheet
+                'visual' => 'EXT:' . $_EXTKEY . '/Resources/Public/Css' // changes default directory
+            ]
+        ];
+    }
+};
+
+$boot($_EXTKEY);
+unset($boot);
